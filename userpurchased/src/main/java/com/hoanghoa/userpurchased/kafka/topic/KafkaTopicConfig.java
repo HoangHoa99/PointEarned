e1f0fe27df5c4176df4d11bb5c1ebeccf9a30acc
HoangHoa99 <hoangthaihoa2122@gmail.com>
2023-06-01 16:01:29 +0700

    kafka demo

---
+++ userpurchased/src/main/java/com/hoanghoa/userpurchased/kafka/topic/KafkaTopicConfig.java
@@ -0,0 +1,16 @@
+package com.hoanghoa.userpurchased.kafka.topic;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic javaguidesTopic(){
+        return TopicBuilder.name("pointearned")
+                .build();
+    }
+}
